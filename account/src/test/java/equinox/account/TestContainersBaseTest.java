550b172775fa69cb047f441d47c32b56801a9fb3
iskorostinskiinikita <nikita11042000@gmail.com>
2025-07-27 18:15:43 +0300

    config fix

---
+++ account/src/test/java/equinox/account/TestContainersBaseTest.java
@@ -0,0 +1,18 @@
+package equinox.account;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Import(OAuthBaseTestConfig.class)
+public class TestContainersBaseTest {
+
+    @Container
+    @ServiceConnection
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
+}
