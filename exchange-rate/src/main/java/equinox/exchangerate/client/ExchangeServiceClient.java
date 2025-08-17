d159e31a488f75742b77596d1760cde46173ebfd
iskorostinskiinikita <nikita11042000@gmail.com>
2025-08-17 16:33:45 +0300

    CI/CD: service discovery (exchange-rate)

--- exchange-rate/src/main/java/equinox/exchangerate/client/ExchangeServiceClient.java
+++ exchange-rate/src/main/java/equinox/exchangerate/client/ExchangeServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value = "exchange", url = "${feign.exchange}")
+@FeignClient(name = "exchange")
 public interface ExchangeServiceClient {
 
     @PostMapping("/api/v1/exchange/updateExchangeRate")
