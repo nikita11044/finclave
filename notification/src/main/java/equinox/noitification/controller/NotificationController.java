589633d85964f39ba1f1952ed2f895494a6e2a9f
iskorostinskiinikita <nikita11042000@gmail.com>
2025-08-28 20:12:10 +0300

    feat: kafka for notifications

--- notification/src/main/java/equinox/noitification/controller/NotificationController.java
+++ notification/src/main/java/equinox/noitification/controller/NotificationController.java
@@ -1,12 +1,9 @@
 package equinox.noitification.controller;
 
-import equinox.noitification.model.dto.NotificationDto;
 import equinox.noitification.service.NotificationsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +14,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NotificationController {
     private final NotificationsService notificationService;
-
-    @PostMapping
-    public void createNotification(@RequestBody NotificationDto dto) {
-        notificationService.createNotification(dto);
-    }
 
     @GetMapping("/{login}")
     public List<String> getAllByLogin(@PathVariable("login") String login) {
--- notification/src/main/java/equinox/noitification/service/NotificationsService.java
+++ notification/src/main/java/equinox/noitification/service/NotificationsService.java
@@ -1,12 +1,19 @@
 package equinox.noitification.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import equinox.noitification.jpa.NotificationRepository;
 import equinox.noitification.model.dto.NotificationDto;
 import equinox.noitification.model.entity.Notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.kafka.annotation.DltHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.kafka.retrytopic.DltStrategy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,14 +24,31 @@ import java.util.List;
 @RequiredArgsConstructor
 public class NotificationsService {
     private final NotificationRepository notificationRepository;
+    private final ObjectMapper objectMapper;
 
-    @Transactional
-    public void createNotification(NotificationDto dto) {
-        notificationRepository.insert(
-                dto.getNotificationId(),
-                dto.getLogin(),
-                dto.getMessage()
-        );
+    @RetryableTopic(
+            attempts = "5",
+            backoff = @Backoff(delay = 1_00, multiplier = 2, maxDelay = 8_000),
+            retryTopicSuffix = "-retry",
+            dltTopicSuffix = "-dlt",
+            dltStrategy = DltStrategy.FAIL_ON_ERROR
+    )
+    @KafkaListener(topics = "notifications", containerFactory = "customKafkaListenerContainerFactory")
+    public void create(String dto) {
+        try {
+            NotificationDto notificationDto = objectMapper.readValue(dto, NotificationDto.class);
+            notificationRepository.insert(
+                    notificationDto.getNotificationId(),
+                    notificationDto.getLogin(),
+                    notificationDto.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DltHandler
+    public void handleDltMessage(ConsumerRecord<?, ?> record) {
+        System.out.println("Message in DLT: " + record.value());
     }
 
     @Transactional(readOnly = true)
